4232b326f5b95c4b78903682b2f57ee64767d7b6
Diego G Plata <diegogplata@gmail.com>
2021-02-10 22:38:47 -0600

    🗃add first migrations for Item model✅add new test for testing database, test db working🚧work in progress

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
 #from django.test import TestCase
 import pytest
-from django.urls import resolve, reverse
+from django.urls import resolve
 from lists.views import home_page
+from lists.models import Item
 
 # Create your tests here.
 
@@ -9,3 +10,17 @@ def test_home_page(client):
     found = resolve('/')
     #response = client.get(url)
     assert found.func == home_page
+
+def test_saving_and_retrieving_items(db):
+    first_item = Item.objects.create()
+    first_item.text = 'First item ever'
+    first_item.save()
+    #second_item = Item()
+    #second_item.text = 'Item two'
+    #second_item.save()
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 1
+    #first_saved_item = saved_items[0]
+    #second_saved_item = saved_items[1]
+    #assert first_saved_item.text == 'First item ever'
+    #assert second_saved_item.text == 'Item two'
